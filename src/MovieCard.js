b0ff3ee18e15fc7a2beb11b8a9344bbed808a15c
root <st43355@student.upce.cz>
2020-05-04 20:45:24 +0000

    Changed components to stateless functional components.

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class MovieCard extends Component {
-  render() {
-    /*
+function MovieCard({ users, usersWhoLikedMovie, movieInfo }) {
+  /*
     Destructuring via ES6. We're getting the profiles, users, and movies properties
     off of the pros passed into this presentational component. If you need a refresher on this syntax, check
     out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
     */
-    const { users, usersWhoLikedMovie, movieInfo } = this.props;
-
     return (
       <li key={movieInfo.id}>
         <h2>{movieInfo.name}</h2>
@@ -30,7 +27,6 @@ class MovieCard extends Component {
         )}
       </li>
     );
-  }
 }
 
 export default MovieCard;
--- src/MovieCardsList.js
+++ src/MovieCardsList.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MovieCard from './MovieCard';
 
-class MovieCardsList extends Component {
-  render() {
+function MovieCardsList({ profiles, users, movies }) {
     /*
-  Destructuring via ES6. We're getting the profiles, users, and movies properties
-  off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-  out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-  */
+    Destructuring via ES6. We're getting the profiles, users, and movies properties
+    off of the pros passed into this presentational component. If you need a refresher on this syntax, check
+    out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
+    */
 
-    const { profiles, users, movies } = this.props;
     const usersByMovie = {};
 
     profiles.forEach(profile => {
@@ -23,19 +21,13 @@ class MovieCardsList extends Component {
     });
 
     const movieCards = Object.keys(movies).map(id => (
-      <MovieCard
-        key={id}
-        users={users}
-        usersWhoLikedMovie={usersByMovie[id]}
-        movieInfo={movies[id]}
-      />
-    ));
+      <MovieCard key={id} users={users} usersWhoLikedMovie={usersByMovie[id]} movieInfo={movies[id]}/>
+  	));
 
-    /*
-    Return JSX
-    */
-    return <ul>{movieCards}</ul>;
-  }
+  	/*
+      Return JSX
+     */
+  	return <ul>{movieCards}</ul>;
 }
 
 export default MovieCardsList;
