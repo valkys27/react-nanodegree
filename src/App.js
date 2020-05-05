4c32fe09317f9a9752f51c870d5f4449c2c36de2
root <st43355@student.upce.cz>
2020-05-05 20:01:51 +0000

    Completed state mirror.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    text: ''
+  }
+
+  onChangeText = event => {
+    this.updateState(event.target.value);
+  }
+
+  updateState(text) {
+    this.setState(currentState => ({
+      text: text
+    }));
+  }
+  
   render() {
     return (
       <div className="App">
@@ -11,9 +26,9 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input type="text" placeholder="Say Something" onChange={this.onChangeText}/>
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.text}</p>
         </div>
       </div>
     );
